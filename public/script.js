e804bc91dee9680025a355f9fa4e103db5830d20
fnumono <wangpramono@gmail.com>
2017-09-24 03:54:10 -0700

    adding tab

--- public/script.js
+++ public/script.js
@@ -5,6 +5,24 @@ window.onload = function() {
 
     var previousMarkdownValue;
 
+    pad.addEventListener('keydown', function(e){
+    	if (e.keyCode === 9){
+    		var start = this.selectionStart;
+    		var end = this.selectionEnd;
+
+    		var target = e.target;
+    		var value = target.value;
+
+    		target.value = value.substring(0, start)
+                            + "\t"
+                            + value.substring(end);
+
+             this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+    	}
+    });
 
     var convertTextAreaToMarkdown = function(){
         var markdownText = pad.value;
